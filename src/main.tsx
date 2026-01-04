981d0c38ea64cd95ec16f945ce982f5b72d7ccc0
Andrey Gaponik <andreygaponik@gmail.com>
2026-01-04 19:48:48 +0300

    new test

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
-      <div>test</div>
+      <div>test 123</div>
     </Provider>
   </StrictMode>,
 );
